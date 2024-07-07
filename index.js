48cf4b225aaf6e7fed471e169a5f2ee8c310c390
Raghav Yadav <isitraghav@gmail.com>
2024-07-08 00:34:42 +0530

    init

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
-const requestIp = require("request-ip");
 const { rateLimit } = require("express-rate-limit");
 const express = require("express");
-const { default: axios } = require("axios");
 const cache = require("memory-cache");
 const app = express();
 const cors = require("cors");
-var _ = require("underscore");
+require("dotenv").config();
 
 app.use(require("sanitize").middleware);
 app.use(express.json());
@@ -18,7 +16,6 @@ app.use(
 );
 
 app.set("trust proxy", 1);
-app.get("/ip", (request, response) => response.json({ ip: request.ip }));
 
 app.all("/", function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
@@ -36,28 +33,10 @@ const limiter = rateLimit({
 // Apply the rate limiting middleware to all requests
 app.use(limiter);
 
-app.get("/api/cache", async (req, res) => {
-  let data = req.query;
-  if (_.has(data, "pub")) {
-    cache.del(data.pub);
-  }
-  res.json({});
-});
-
-app.get("/api/clearallcache", async (req, res) => {
-  let data = req.query;
-  if (_.has(data, "pass")) {
-    if (data.pass == "Raghav1979") {
-      res.json(JSON.parse(cache.exportJson()));
-      cache.clear();
-      return;
-    }
-  }
-  res.json({
-    m: "wrong or no password",
-  });
+app.get("/", function (req, res) {
+  res.send("Hello World");
 });
 
 app.listen(process.env.PORT || 3000);
-
+console.log("Listening on: http://localhost:" + (process.env.PORT || 3000));
 module.exports = app;
