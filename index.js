9b91a9d5bbe2d27b975b7a30415ccd2259bbf6db
Shrey <jshreyj19@gmail.com>
2025-05-14 18:38:29 +0530

    init

--- index.js
+++ index.js
@@ -4,37 +4,11 @@ const cache = require("memory-cache");
 const app = express();
 const cors = require("cors");
 require("dotenv").config();
+app.use(express.static(__dirname));
 
-app.use(require("sanitize").middleware);
-app.use(express.json());
-app.use(
-  cors({
-    origin: "*",
-    allowedHeaders: "*",
-    preflightContinue: true,
-  })
-);
-
-app.set("trust proxy", 1);
-
-app.all("/", function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With");
-  next();
-});
-
-const limiter = rateLimit({
-  windowMs: 1 * 60 * 1000, // 1 minute
-  limit: 15,
-  standardHeaders: "draft-7",
-  legacyHeaders: false,
-});
-
-// Apply the rate limiting middleware to all requests
-app.use(limiter);
-
-app.get("/", function (req, res) {
-  res.send("Hello World");
+// Serve index.html on root
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 app.listen(process.env.PORT || 3000);
